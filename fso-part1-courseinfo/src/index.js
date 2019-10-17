9fb846e2cebb4df0c343b914c97779571d0ae474
Demola John <29120743+ceofvo@users.noreply.github.com>
2019-10-17 13:11:47 +0100

    Add files via upload

---
+++ fso-part1-courseinfo/src/index.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const Header = (props) => {
+    console.log(props, "Header");
+    return(
+        <div>
+            <h1>{props.course.name}</h1>
+        </div>
+    );
+ };
+
+
+ const Part = (props) => {
+    console.log(props, "Parts");
+     return (
+         <div>
+            <p>{props.part} {props.exercises}</p>      
+         </div>
+     );
+ };
+ 
+
+const Content = (props) => {
+    console.log(props, "Content");
+    return (
+        <div>
+            <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+            <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+            <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
+        </div>
+    );
+};
+
+const Total = (props) => {
+    console.log(props, "Total");
+    return (
+        <div>
+        <p>Number of exercises {
+            props.parts[0].exercises + 
+            props.parts[1].exercises + 
+            props.parts[2].exercises}</p>
+        </div>
+    );
+};
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7
+          },
+          {
+            name: 'State of a component',
+            exercises: 14
+          }
+        ]
+      }
+  
+    return (
+      <div>
+        <Header course={course} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    );
+  };
+  
+
+ReactDOM.render(<App />, document.getElementById('root'));
